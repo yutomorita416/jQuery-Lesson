de8d990083776143e9659949b32729c40e532bbc
YutoMorita <yutomorita416@gmail.com>
2021-05-03 15:39:00 +0900

    finish js-7

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-$(function (){
-  $('.box1').slideDown(2000, function (){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'height': '200px',
-      'width': '200px'
-    }).slideUp(2000);
+$(function(){
+  $('.box1').on('click', function(){
+    $(this).slideUp();
   });
 });
 
 $(function(){
-  $('.box2').mouseover(function(){
-    $('.box2').addClass('box2-ext');
-  });
-  $('.box2').mouseout(function(){
-    $('.box2').removeClass('box2-ext');
-  });
-});
-
-$(function(){
-  $('.box3').on('click', function(){
-    $('.box3').addClass('box3-ext');
-  });
-  $('.box3').mouseout(function(){
-    $('.box3').removeClass('box3-ext');
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
   });
 });
