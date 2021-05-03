66aad073bb47d9c7545d803bc96ad4ac7434458a
YutoMorita <yutomorita416@gmail.com>
2021-05-03 15:22:47 +0900

    finish js-6

--- app.js
+++ app.js
@@ -7,3 +7,21 @@ $(function (){
     }).slideUp(2000);
   });
 });
+
+$(function(){
+  $('.box2').mouseover(function(){
+    $('.box2').addClass('box2-ext');
+  });
+  $('.box2').mouseout(function(){
+    $('.box2').removeClass('box2-ext');
+  });
+});
+
+$(function(){
+  $('.box3').on('click', function(){
+    $('.box3').addClass('box3-ext');
+  });
+  $('.box3').mouseout(function(){
+    $('.box3').removeClass('box3-ext');
+  });
+});
