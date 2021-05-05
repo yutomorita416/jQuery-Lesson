59d99762df7a9b5d78d40b050c350cc013d2414c
YutoMorita <yutomorita416@gmail.com>
2021-05-05 15:16:08 +0900

    finish js-5 2nd

--- app2nd.js
+++ app2nd.js
@@ -1,3 +1,33 @@
-$(document).ready(function () {
-  $("body").html("<h1>Hello jQuery!!</h1>");
+$(function () {
+  $('.box1').css({
+    'background-color': '#0000ff',
+    'height': '100px'
+  });
+});
+
+$(function () {
+  $('.box2').slideDown(500);
+});
+
+$(function () {
+  $('.box3').slideUp(1000);
+});
+
+$(function () {
+  $('.box4').show(500);
+  $('.box4').css({'background-color': '#ffee00'});
+});
+
+$(function () {
+  $('.box5').hide(1000);
+});
+
+$(function () {
+  $('.box6').slideDown(2000, function() {
+    $('.box6').css({
+      'background-color': '#0000ff',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp(2000);
+  });
 });
